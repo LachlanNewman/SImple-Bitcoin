d422da0055d33aec70e41b3f598e78f21b292261
Lachlan Newman <lachnewman007@gmail.com>
2017-05-04 20:40:18 +0800

    commentsand server chat removed

--- peer2peer.py
+++ peer2peer.py
@@ -1,21 +1,28 @@
 import socket
+import sys
 
 class p2p(object):
 
     def __init__(self, host, port):
-        self.host = host
-        self.port = port
+        self.host = host #host name
+        self.port = port #port for server
 
     def connectServer(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #while loop keeps socket open until client disconects
         while True:
             try:
+                #test to see if the port is open
                 self.socket.bind((self.host,self.port))
-                print('port', self.port , 'available' )
+                #if port is open write port number to file csp.txt
+                #csp = open('csp.txt', 'a+')
+                #csp.write(str(self.port)+',')
+                #csp.close()
                 break
             except socket.error as e:
                 if e.errno == 98:
                     print("Port is already in use")
+                    #add 1 to try another port and see if it is open
                     self.port += 1
                 else:
                     print(e)
@@ -23,15 +30,13 @@ class p2p(object):
         self.socket.listen(1)
 
     def acceptClients(self):
+        #allow socket to accept connection from clients
         self.connection, self.address = self.socket.accept()
+        #while loop keeps sockets open until clients leave
         while True:
             data = self.connection.recv(1024).decode()
             if not data:
                 break
             print ("from connected  user: " + str(data))
-
-            data = input("->")
-            print ("sending: " + str(data))
-            self.connection.send(data.encode())
 
         self.connection.close()
