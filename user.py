fd0f2599625be9a57c56c4c1e926471274c72e3b
Lachlan Newman <lachnewman007@gmail.com>
2017-05-04 22:04:02 +0800

    threading changed to process

--- user.py
+++ user.py
@@ -1,30 +1,27 @@
 
 import serverclass
-import threading
+import clientclass
+import multiprocessing
 import time
 
 
 def clientAction():
-    global port
-    shared.acquire()
-    print("client port in use", port)
+
+    print("client port in use")
 
 def serverAction():
-    global port
-    shared.acquire()
     server = serverclass.Server('localhost',8000)
     port = server.port
     print("port in use = ", port)
-    #server.connectServer()
-    #print("socket connected")
-    #server.connectClient()
-    #server.acceptClients()
+    server.connectServer()
+    print("socket connected")
+    server.connectClient()
+    server.acceptClients()
 
-shared = threading.Condition()
-client = threading.Thread(target=clientAction)
-server = threading.Thread(target=serverAction)
+
+client = multiprocessing.Process(target=clientAction)
+server = multiprocessing.Process(target=serverAction)
 server.start()
-time.sleep(1)
 client.start()
 client.join()
 server.join()
