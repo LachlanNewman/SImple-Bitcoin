cebb458b006e540cc954e99e4fa549b18b19edb4
cg146 <chenmeng.627@gmail.com>
2017-05-15 20:39:06 +0800

    Add files via upload
    store blocks in a database file

---
+++ blockchain_v2/test.py
@@ -0,0 +1,21 @@
+#Karl CHEN 15.5.2017
+
+import blockclass_v2
+
+#create the first block
+blockclass_v2.firstblock()
+
+#create transaction dict with transaction detail
+t = blockclass_v2.trans(100,'alice','bob')
+ti = t.createtrans()
+
+#create block dict with block detail
+b=blockclass_v2.block('2342',123101,ti)
+bi = b.createblock()
+
+#add block to block chain
+b.addblock()
+
+#get block info by the height
+b.getblockinfo()
+b.getblockinfo()
