38ebef3fa5d3c090f384c5cc65b5f6735bfcec4e
Lachlan Newman <lachnewman007@gmail.com>
2017-05-24 00:56:40 +0800

    rewrite server and client as ssl and not p2p

--- P2P Network/testclient.py
+++ P2P Network/testclient.py
@@ -1,18 +1,95 @@
-import socket
+import socket, ssl, pprint, uuid, subprocess, json
 
-host = 'localhost' # '127.0.0.1' can also be used
-port1 = 7001
-port2 = 7002
+#------------------------------------------------------------------------------------------------------------------------------------------
+#sends all transactions to server TODO EXPLAIN MORE
+#------------------------------------------------------------------------------------------------------------------------------------------
+def sendTransaction(ssl_sock,userid):
+    while True:
+        transaction = {}                        #empty transaction
+        src         = userid                    #auto userid of sender
+        dest        = input("id:"       )       #user id of reciever
+        amount      = input("amount:"   )       #amount to transfer
+        comment     = input("comment:"  )       #comment to go with transaction
+        if src == "X" and dest =="X" and amount = "X":          #condition to end socket connection TODO add to readme file
+            break;
+        ssl_sock.write(message.encode())        #send transaction to server
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Connecting to socket
-sock.connect((host, port1)) #Connect takes tuple of host and port
-sock2.connect((host, port2)) #Connect takes tuple of host and port
-#Infinite loop to keep client running.
-while True:
-    message = input('->')
-    sock.send(message.encode())
-    sock2.send(message.encode())
+#------------------------------------------------------------------------------------------------------------------------------------------
+#get all the publickeys of users in the network #TODO explain
+#------------------------------------------------------------------------------------------------------------------------------------------
+def getPublicKeys(ssl_sock,networkSize):
+    while networkSize != 0 :
+        publickeys = ssl_sock.recv(1024)    #recieve public key of user from server
+        if not publickeys:
+            print("waiting for public keys")
+            continue
+        else:
+            #print(publickeys.decode())     #debug
+            networkSize = networkSize -1    #if the network size counter reaches zero all of the pubic keys have been recieved
+#------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
 
-sock.close()
+#------------------------------------------------------------------------------------------------------------------------------------------
+#get the public keys from the file TODO explain more
+#------------------------------------------------------------------------------------------------------------------------------------------
+def getPublicKey(user):
+    public = ""
+    public_file = open("public"+user+".pem", 'r')
+    for line in public_file:
+        public = public + line
+    public_file.close()
+    return public
+#------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+#genRSAKeyPairs #TODO inport this function from isaccs crypto class
+#------------------------------------------------------------------------------------------------------------------------------------------
+def genRSAKeyPairs(user):
+    #Use OpenSSL to generate given bit modulus RSA keys NOT using Triple Data Encryption (-des3)
+    subprocess.call(["openssl", "genrsa", "-out", "private" + user + ".pem", "2048"])
+
+    #Write public key out to new file named publicX.pem
+    subprocess.call(["openssl", "rsa", "-in", "private" + user + ".pem", "-outform", "PEM", "-pubout", "-out", "public" + user + ".pem"])
+#------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+def createSSLSocket():
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Require a certificate from the server. We used a self-signed certificate
+        # so here ca_certs must be the server certificate itself.
+        ssl_sock = ssl.wrap_socket(s,
+                                   ca_certs="mycert.pem",
+                                   cert_reqs=ssl.CERT_REQUIRED)
+
+        ssl_sock.connect(('localhost', 10000))
+        return ssl_sock
+#------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+#MAIN
+#------------------------------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    networkSize = 2 #TODO make input at runtime
+
+    ssl_sock  = createSSLSocket()        #create a ssl socket connection to server
+    userid    = str(uuid.uuid4())        #generate a unique userid
+    genRSAKeyPairs(userid)               #generate a public and private key for user
+    publickey = getPublicKey(userid)     #get the publickey of the user from the file publickey<userid>.pem
+
+
+    userinfo['id']        = userid
+    userinfo['publickey'] = publickey
+
+    ssl_sock.write (json.dumps(userinfo).encode()) #send the publickey and the user id to the server
+    getPublicKeys  (ssl_sock  ,networkSize)       #get the public keys from all users in the network
+    sendTransaction(ssl_sock  ,userid     )       #send a transation to another user
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
--- P2P Network/testserver.py
+++ P2P Network/testserver.py
@@ -1,34 +1,61 @@
-import socket
-#Importing all from thread
-import threading
+import socket, ssl, threading, json, time
 
-# Defining server address and port
-host = ''  #'localhost' or '127.0.0.1' or '' are all same
-#port = 7001 #Use port > 1024, below it all are reserved
-port = 7002
+if __name__ == '__main__':
 
-#Creating socket object
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Binding socket to a address. bind() takes tuple of host and port.
-sock.bind((host, port))
-#Listening at the address
-sock.listen(5) #5 denotes the number of clients can queue
+    socket = socket.socket() #create socket
+    socket.bind(('', 10000)) #bind socket to port and host TODO make over network and check if port is open
+    socket.listen(5)         #listen to a que of connections
+    num_clients = 2          #TODO amke num of user input from command line
+    clientConnections = []   #list of connection detials for each client to server
+    clientPublicKeys  = []   #list of the publickeys for each client
+    networkSize       = 0    #initailse the number of users in the network
 
-def clientthread(conn):
-#infinite loop so that function do not terminate and thread do not end.
-     while True:
-        data = conn.recv(1024).decode() # 1024 stands for bytes of data to be received
-        if not data:
-            continue
-        else:
-            print("from thread:" , threading.current_thread(), data)
-        #allow socket to accept connection from clients
+    #------------------------------------------------------------------------------------------------------------------------------------------
+    #Send the public keys of the users in the netwrok to every user
+    #------------------------------------------------------------------------------------------------------------------------------------------
+    def sendPublicKeys(clientConnections):
+        #print("sending public keys")
+        for conn in clientConnections:                  #for each socket connection between a client
+            for client in clientPublicKeys:             #send each clients public key
+                conn.send((json.dumps(client).encode()))
+    #------------------------------------------------------------------------------------------------------------------------------------------
+    #------------------------------------------------------------------------------------------------------------------------------------------
 
-while True:
-    connection, address = sock.accept()
-    #Creating new thread. Calling clientthread function for this function and passing conn as argument.
-    client = threading.Thread(target=clientthread,args=(connection,)) #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    client.start()
 
-connection.close()
-socket.close
+    #------------------------------------------------------------------------------------------------------------------------------------------
+    #runs in a seperate thread to enable mutiple clients to connecto to the server
+    #------------------------------------------------------------------------------------------------------------------------------------------
+    def clientthread(connstream,networkSize):
+        #infinite loop so that function do not terminate and thread do not end.
+        print(networkSize)
+        while True:
+            data = connstream.read().decode()   # 1024 stands for bytes of data to be received
+            if not data:
+                break
+            try:
+                data = json.loads(data)
+                clientPublicKeys.append(data)   #append the public key to the list of public keys TODO condition to check if pubic keys or transaction
+            except:
+                #print(data)
+        connstream.close()  #close the socket connection
+    #------------------------------------------------------------------------------------------------------------------------------------------
+    #------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+    while True:
+        newsocket, fromaddr = socket.accept() #initialise servers socket
+        #ensure that all client who connect recieve ssl certificate
+        connstream = ssl.wrap_socket(newsocket, server_side=True,
+                                     certfile="mycert.pem",
+                                     keyfile="mycert.pem")
+
+        networkSize = networkSize + 1                   #number of users in the network increases by ! for each connection
+        clientConnections.append(connstream)            #append the details of the connection to the connection list
+        client = threading.Thread(target=clientthread,  #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+                        args=(connstream,networkSize))
+        client.start()                                  #start thread for client
+
+        if(networkSize == num_clients):                 #if the network size reaches maximum TODO make input for runtime of maximum network size
+            time.sleep(3)                               #ensures final public key is added to publickeys list
+            sendPublicKeys(clientConnections)           #send the public keys to all clients
