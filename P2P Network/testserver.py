fc02c1bf5af698fcaf7b6e603727d9f83e74e93f
Lachlan Newman <lachnewman007@gmail.com>
2017-05-24 17:56:34 +0800

    indentation error fixed

--- P2P Network/testserver.py
+++ P2P Network/testserver.py
@@ -1,48 +1,14 @@
 import socket, ssl, threading, json, time
 
+socket = socket.socket() #create socket
+socket.bind(('', 10000)) #bind socket to port and host TODO make over network and check if port is open
+socket.listen(5)         #listen to a que of connections
+num_clients = 2          #TODO amke num of user input from command line
+clientConnections = []   #list of connection detials for each client to server
+clientPublicKeys  = []   #list of the publickeys for each client
+networkSize       = 0    #initailse the number of users in the network
+
 if __name__ == '__main__':
-
-    socket = socket.socket() #create socket
-    socket.bind(('', 10000)) #bind socket to port and host TODO make over network and check if port is open
-    socket.listen(5)         #listen to a que of connections
-    num_clients = 2          #TODO amke num of user input from command line
-    clientConnections = []   #list of connection detials for each client to server
-    clientPublicKeys  = []   #list of the publickeys for each client
-    networkSize       = 0    #initailse the number of users in the network
-
-    #------------------------------------------------------------------------------------------------------------------------------------------
-    #Send the public keys of the users in the netwrok to every user
-    #------------------------------------------------------------------------------------------------------------------------------------------
-    def sendPublicKeys(clientConnections):
-        #print("sending public keys")
-        for conn in clientConnections:                  #for each socket connection between a client
-            for client in clientPublicKeys:             #send each clients public key
-                conn.send((json.dumps(client).encode()))
-    #------------------------------------------------------------------------------------------------------------------------------------------
-    #------------------------------------------------------------------------------------------------------------------------------------------
-
-
-    #------------------------------------------------------------------------------------------------------------------------------------------
-    #runs in a seperate thread to enable mutiple clients to connecto to the server
-    #------------------------------------------------------------------------------------------------------------------------------------------
-    def clientthread(connstream,networkSize):
-        #infinite loop so that function do not terminate and thread do not end.
-        print(networkSize)
-        while True:
-            data = connstream.read().decode()   # 1024 stands for bytes of data to be received
-            if not data:
-                break
-            try:
-                data = json.loads(data)
-                clientPublicKeys.append(data)   #append the public key to the list of public keys TODO condition to check if pubic keys or transaction
-            except:
-                #print(data)
-        connstream.close()  #close the socket connection
-    #------------------------------------------------------------------------------------------------------------------------------------------
-    #------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
     while True:
         newsocket, fromaddr = socket.accept() #initialise servers socket
         #ensure that all client who connect recieve ssl certificate
@@ -59,3 +25,34 @@ if __name__ == '__main__':
         if(networkSize == num_clients):                 #if the network size reaches maximum TODO make input for runtime of maximum network size
             time.sleep(3)                               #ensures final public key is added to publickeys list
             sendPublicKeys(clientConnections)           #send the public keys to all clients
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+#Send the public keys of the users in the netwrok to every user
+#------------------------------------------------------------------------------------------------------------------------------------------
+def sendPublicKeys(clientConnections):
+    #print("sending public keys")
+    for conn in clientConnections:                  #for each socket connection between a client
+        for client in clientPublicKeys:             #send each clients public key
+            conn.send((json.dumps(client).encode()))
+#------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+#runs in a seperate thread to enable mutiple clients to connecto to the server
+#------------------------------------------------------------------------------------------------------------------------------------------
+def clientthread(connstream,networkSize):
+    #infinite loop so that function do not terminate and thread do not end.
+    print(networkSize)
+    while True:
+        data = connstream.read().decode()       # 1024 stands for bytes of data to be received
+        if not data:
+            break
+        data = json.loads(data)
+        if data['header'] == "publickey":
+            clientPublicKeys.append(data)   #append the public key to the list of public keys TODO condition to check if pubic keys or transaction
+        elif data['header'] == "transaction":
+            print("transaction recieved")
+        else:
+            print("not a recognised heaeder message ignored")
